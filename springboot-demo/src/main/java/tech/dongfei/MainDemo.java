1fee078098d64ab9411519331e7aabc855ce664f
dongfei <652117746@qq.com>
2022-03-29 22:00:30 +0800

    Init 2022-3-29 22:00:24

---
+++ springboot-demo/src/main/java/tech/dongfei/MainDemo.java
@@ -0,0 +1,11 @@
+package tech.dongfei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(MainDemo.class, args);
+    }
+}
